e0903c946d8ac63fd0dd8864df45bfeaeea7be59
Chakshu Jain <mrck572@gmail.com>
2020-07-30 22:00:42 +0530

    Updates

---
+++ precache-manifest.a3e2e0a82b6e5a4354086c6006b94d92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc34c748e9dc4cde952462fee9a81c35",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1bed17e0da6edf837a8e",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "45ac865879f0aef1680f",
+    "url": "/portfolio/static/css/main.16899f4a.chunk.css"
+  },
+  {
+    "revision": "1bed17e0da6edf837a8e",
+    "url": "/portfolio/static/js/2.11042ac6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.11042ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ac865879f0aef1680f",
+    "url": "/portfolio/static/js/main.dd7d349d.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "be47e3b6c93aefd3088602f8aa0f6f1e",
+    "url": "/portfolio/static/media/mypic.be47e3b6.jpg"
+  }
+]);
